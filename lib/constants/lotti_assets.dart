1aa2d88fbc912169eae187eab67d76b9797469cc
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-07 00:25:38 -0500

    Added assets package

--- lib/constants/lotti_assets.dart
+++ lib/constants/lotti_assets.dart
@@ -1,9 +1,9 @@
 class LottieAssets {
-  static const scannerQR = 'assets/lottie/scanner_qr.json';
-  static const food = 'assets/lottie/food.json';
-  static const error = 'assets/lottie/error.json';
-  static const sailor = 'assets/lottie/sailor.json';
-  static const login = 'assets/lottie/login.json';
-  static const ordering = 'assets/lottie/ordering.json';
-  static const preparingFood = 'assets/lottie/preparing-food.json';
+  static const scannerQR = 'packages/oyt_front_core/assets/lottie/scanner_qr.json';
+  static const food = 'packages/oyt_front_core/assets/lottie/food.json';
+  static const error = 'packages/oyt_front_core/assets/lottie/error.json';
+  static const sailor = 'packages/oyt_front_core/assets/lottie/sailor.json';
+  static const login = 'packages/oyt_front_core/assets/lottie/login.json';
+  static const ordering = 'packages/oyt_front_core/assets/lottie/ordering.json';
+  static const preparingFood = 'packages/oyt_front_core/assets/lottie/preparing-food.json';
 }
