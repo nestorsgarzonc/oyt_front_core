5e2ef4aa8f33fd61a1a78d517099b9d6a635f073
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-07 00:07:03 -0500

    Improved theme and added constants

--- lib/constants/socket_constants.dart
+++ lib/constants/socket_constants.dart
@@ -1,13 +1,23 @@
 class SocketConstants {
-  static const joinSocket = 'table:join';
-  static const onNewUserJoined = 'new_user_joined';
   static const addToOrder = 'order:add-item';
-  static const listOfOrders = 'list_of_orders';
   static const callWaiter = 'table:call-waiter';
-  static const stopCallWaiter = 'table:stop-calling-waiter';
-  static const orderNow = 'table:order-now';
-  static const deleteItem = 'order:delete-item';
   static const changeTableStatus = 'table:change-status';
+  static const deleteItem = 'order:delete-item';
   static const editItem = 'order:edit-item';
+  static const joinSocket = 'table:join';
   static const listenOnPay = 'on_payed_account';
+  static const listOfOrders = 'list_of_orders';
+  static const onNewUserJoined = 'new_user_joined';
+  static const orderNow = 'table:order-now';
+  static const stopCallWaiter = 'table:stop-calling-waiter';
+
+  //WAITER
+  static const addToOrderToUser = 'waiter:add-item-to-table';
+  static const customerRequests = 'customer_requests';
+  static const joinToRestaurant = 'restaurant:join';
+  static const leaveTable = 'waiter:leave-table';
+  static const listenTables = 'restaurant:tables';
+  static const orderQueue = 'order_queue';
+  static const watchATable = 'watch-table';
+  static const watchTable = 'waiter:watch-table';
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTheme {
   static ThemeData myTheme() {
     return ThemeData(
       primarySwatch: Colors.deepOrange,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.deepOrange,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          elevation: 2,
+        ),
+      ),
+      cardTheme: CardTheme(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        color: Colors.white,
+        surfaceTintColor: Colors.white,
+      ),
+      listTileTheme: ListTileThemeData(
+        tileColor: Colors.white.withOpacity(0.9),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      ),
       appBarTheme: const AppBarTheme(
-        backgroundColor: Colors.white,
         elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle.dark,
         iconTheme: IconThemeData(color: Colors.black),
         titleTextStyle: TextStyle(
           color: Colors.black,
-          fontSize: 17,
+          fontSize: 18,
           fontWeight: FontWeight.w600,
         ),
       ),
