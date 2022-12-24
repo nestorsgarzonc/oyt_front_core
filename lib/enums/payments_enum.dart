b9b3ffb5232e6a80b79492d148bec0f9d9c3598e
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-23 23:58:25 -0500

    Added payments enum

---
+++ lib/enums/payments_enum.dart
@@ -0,0 +1,34 @@
+enum PaymentMethod {
+  cash(title: 'Efectivo', paymentValue: 'cash'),
+  card(title: 'Tarjeta credito', paymentValue: 'card'),
+  pse(title: 'PSE', paymentValue: 'pse');
+
+  const PaymentMethod({required this.title, required this.paymentValue});
+
+  final String title;
+  final String paymentValue;
+}
+
+enum PaymentWay {
+  all(title: 'Pago total', paymentValue: 'all'),
+  split(title: 'Pago individual', paymentValue: 'split');
+
+  const PaymentWay({required this.title, required this.paymentValue});
+
+  final String paymentValue;
+  final String title;
+}
+
+enum PaymentTip {
+  fiveteen(title: '15%', value: 15),
+  ten(title: '10%', value: 10),
+  five(title: '5%', value: 5),
+  none(title: 'Sin propina', value: 0);
+
+  const PaymentTip({required this.title, required this.value});
+
+  num calculateTip(num total) => total * (value / 100);
+
+  final String title;
+  final int value;
+}
