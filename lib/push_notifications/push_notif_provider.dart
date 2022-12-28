890465111c4a00cc953ed745381a539a31e050c5
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2022-12-28 10:31:38 -0500

    Added provider for notifications

--- lib/push_notifications/push_notif_provider.dart
+++ lib/push_notifications/push_notif_provider.dart
@@ -5,10 +5,22 @@ import 'package:oyt_front_core/logger/logger.dart';
 class PushNotificationProvider {
   static Future<void> setupInteractedMessage() async {
     await FirebaseMessagingUtils.requestPermissions();
+    listenMessages();
     RemoteMessage? initialMessage = await FirebaseMessaging.instance.getInitialMessage();
     if (initialMessage != null) {
       FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
     }
+  }
+
+  static void listenMessages() {
+    Logger.log('Listening for messages');
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      Logger.log('Got a message whilst in the foreground!');
+      Logger.log('Message data: ${message.data}');
+      if (message.notification != null) {
+        Logger.log('Message also contained a notification: ${message.notification}');
+      }
+    });
   }
 
   static void _handleMessage(RemoteMessage message) {
