5d1fe28f1889b83bc5c043c4807e97a46c0df1f9
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-12 23:58:45 -0500

    Imptoved push notifications

--- lib/push_notifications/push_notif_provider.dart
+++ lib/push_notifications/push_notif_provider.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:oyt_front_core/constants/firebase_constants.dart';
 import 'package:oyt_front_core/firebase_messaging.dart';
 import 'package:oyt_front_core/logger/logger.dart';
 
@@ -28,14 +29,17 @@ class PushNotificationProvider {
   }
 
   Future<void> setupInteractedMessage() async {
-    await FirebaseMessagingUtils.requestPermissions();
+    await Future.wait([
+      FirebaseMessagingUtils.requestPermissions(),
+      FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+        alert: true,
+        badge: true,
+        sound: true,
+      ),
+      FirebaseMessaging.instance.getToken(vapidKey: FirebaseConstants.vapidKey),
+    ]);
+    final initialMessage = await FirebaseMessaging.instance.getInitialMessage();
     listenMessages();
-    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-      alert: true,
-      badge: true,
-      sound: true,
-    );
-    RemoteMessage? initialMessage = await FirebaseMessaging.instance.getInitialMessage();
     if (initialMessage != null) {
       FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
     }
