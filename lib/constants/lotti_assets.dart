22985374acdce9077009b91541ae294ec2d428a8
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-17 12:49:42 -0500

    Added more assets

--- lib/constants/lotti_assets.dart
+++ lib/constants/lotti_assets.dart
@@ -7,4 +7,12 @@ class LottieAssets {
   static const ordering = 'packages/oyt_front_core/assets/lottie/ordering.json';
   static const preparingFood = 'packages/oyt_front_core/assets/lottie/preparing-food.json';
   static const uploadFile = 'packages/oyt_front_core/assets/lottie/photo-files-upload.json';
+  static const adminFancy = 'packages/oyt_front_core/assets/lottie/admin_fancy.json';
+  static const adminCrm = 'packages/oyt_front_core/assets/lottie/admin-crm.json';
+  static const admin = 'packages/oyt_front_core/assets/lottie/admin.json';
+  static const analytics = 'packages/oyt_front_core/assets/lottie/analytics.json';
+  static const appAnimation = 'packages/oyt_front_core/assets/lottie/app-animation.json';
+  static const chefPreparingFood = 'packages/oyt_front_core/assets/lottie/chef-preparing-food.json';
+  static const cooking = 'packages/oyt_front_core/assets/lottie/cooking.json';
+  static const truckLeavingMerch = 'packages/oyt_front_core/assets/lottie/truck-leaving-merch.json';
 }
