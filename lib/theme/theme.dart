62d461e91018200c07ef35ebebdefa73292fd364
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2023-01-24 21:07:43 -0500

    Added theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,40 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class CustomTheme {
-  static ThemeData myTheme() {
-    return ThemeData(
-      useMaterial3: true,
-      colorSchemeSeed: Colors.deepOrange,
-      listTileTheme: const ListTileThemeData(
-        minLeadingWidth: 5,
-        horizontalTitleGap: 8,
+  static final ThemeData myTheme = ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: Colors.deepOrange,
+    listTileTheme: const ListTileThemeData(
+      minLeadingWidth: 5,
+      horizontalTitleGap: 8,
+    ),
+    appBarTheme: const AppBarTheme(
+      systemOverlayStyle: SystemUiOverlayStyle.dark,
+      iconTheme: IconThemeData(color: Colors.black),
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
       ),
-      appBarTheme: const AppBarTheme(
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
-        iconTheme: IconThemeData(color: Colors.black),
-        titleTextStyle: TextStyle(
-          color: Colors.black,
-          fontSize: 18,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-      visualDensity: VisualDensity.adaptivePlatformDensity,
-    );
-  }
+    ),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
 
   // [TEXT STYLES]
   static const sectionTitleStyle = TextStyle(fontWeight: FontWeight.w600, fontSize: 16);
   static const selectedItemTextStyle =
       TextStyle(fontWeight: FontWeight.w600, color: Colors.deepOrange);
   // [BOX DECORATIONS]
-  static const drawerBoxDecoration = BoxDecoration(
-    color: Colors.white,
-    boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 10)],
+  static final drawerBoxDecoration = BoxDecoration(
+    color: myTheme.cardColor,
+    boxShadow: const [BoxShadow(color: Colors.black12, blurRadius: 10)],
   );
-  static const roundedBoxDecoration = BoxDecoration(
-    color: Colors.white,
-    boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 10)],
-    borderRadius: BorderRadius.all(Radius.circular(10)),
+  static final roundedBoxDecoration = BoxDecoration(
+    color: myTheme.cardColor,
+    boxShadow: const [BoxShadow(color: Colors.black12, blurRadius: 10)],
+    borderRadius: const BorderRadius.all(Radius.circular(10)),
   );
   // [PADDINGS]
   static const drawerBodyPadding = EdgeInsets.symmetric(horizontal: 20);
